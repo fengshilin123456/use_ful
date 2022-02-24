58e63df7c731f09d91a9133ff7bb1fbd860aa2dd
orange <1610728447@qq.com>
2022-02-24 19:13:06 +0800

    '封装aja，引入模板引擎'

---
+++ js/index_tem.js
@@ -0,0 +1,27 @@
+let myajax = (url, move) => {//z 封装ajax，下次直接调函数传不同接口就行
+    let index_ajax = new XMLHttpRequest() || new ActiveXObject(Microsoft.XMLHTTP);//c 定义ajax
+    index_ajax.open('get', url, true); //c get 请求数据
+    index_ajax.send();
+    index_ajax.onreadystatechange = function () {
+        if (index_ajax.readyState == 4 && index_ajax.status == 200) {
+            let pag = index_ajax.responseText;
+            let pags = JSON.parse(pag);
+            console.log(pags);
+            move(pags)
+        }
+    }
+}//v    ajax 一次就好
+let move_one = (data) => {//m 函数名要换
+    let html = template('tem', {//m每次更新要换新ID
+        value: data
+    });
+    document.querySelector('.tem_one .tem_one_ul').innerHTML = html;//m每次换新要换获取的dom
+}
+myajax('http://192.168.31.110:3000/guid/hot', move_one);
+let move_two = (data) => {//m 函数名要换
+    let html = template('tem2', {//m每次更新要换新ID
+        value: data
+    });
+    document.querySelector('.tem_two .tem_two_ul').innerHTML = html;//m每次换新要换获取的dom
+}
+myajax('http://192.168.31.110:3000/report/new', move_two);
