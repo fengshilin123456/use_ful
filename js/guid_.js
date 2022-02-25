3cdb7e139a7fc90a097b1cd3740ed65617bbccf4
shilin <3056127183@qq.com>
2022-02-25 20:40:12 +0800

    'change_热’

---
+++ js/guid_.js
@@ -0,0 +1,32 @@
+function ajax_(url) {
+    let Ajax = new XMLHttpRequest || new ActiveXObject("Micsoft.XMLHTTP");
+    Ajax.open('get', url, true);
+    Ajax.send();
+    Ajax.onreadystatechange = function() {
+        if (Ajax.readyState == 4) {
+            if (Ajax.status == 200) {
+                var res = JSON.parse(Ajax.responseText);
+                render(res)
+            }
+        }
+    }
+}
+
+ajax_('http://192.168.31.110:3000/guid/new');
+
+function render(res) {
+    console.log(res);
+    var html = template("frist", {
+        value: res
+    })
+    document.querySelector('.vess').innerHTML = html;
+}
+
+
+var load = document.querySelector('.load');
+var load_1 = document.querySelector('.load_1')
+load.addEventListener('click', function() {
+    load.style.display = 'none';
+    load_1.style.display = 'block';
+    ajax_('http://192.168.31.110:3000/play/new');
+})
